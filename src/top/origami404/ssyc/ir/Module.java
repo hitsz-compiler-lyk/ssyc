5e2735c87ad1e62c51658c541f055f912488f115
Origami404 <Origami404@foxmail.com>
2022-05-07 05:18:44 +0800

    增加 Module 的方法

--- src/top/origami404/ssyc/ir/Module.java
+++ src/top/origami404/ssyc/ir/Module.java
@@ -1,7 +1,44 @@
 package top.origami404.ssyc.ir;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import top.origami404.ssyc.ir.arg.Function;
+import top.origami404.ssyc.ir.arg.PtrReg;
+
 public class Module {
+    public Module(String name) {
+        this.name = name;
+        this.functions = new HashMap<>();
+        this.globalVars = new HashMap<>();
+    }
+
     public String toTextForm() {
         return "";
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Optional<Function> getFunction(String name) {
+        return Optional.ofNullable(functions.get(name));
+    }
+
+    public void addFunction(Function func) {
+        functions.put(func.getName(), func);
+    }
+
+    public Optional<PtrReg> getGlobalVar(String name) {
+        return Optional.ofNullable(globalVars.get(name));
+    }
+
+    public void addGlobalVar(PtrReg var) {
+        globalVars.put(var.getName(), var);
+    }
+
+    private final String name;
+    private Map<String, Function> functions;
+    private Map<String, PtrReg> globalVars;
 }
