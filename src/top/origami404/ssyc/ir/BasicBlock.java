24db12cef87868f38c55e4f267351953d28f1bc9
Origami404 <Origami404@foxmail.com>
2022-06-23 19:44:16 +0800

    新增分析信息接口 (AnalysisInfo)

--- src/top/origami404/ssyc/ir/BasicBlock.java
+++ src/top/origami404/ssyc/ir/BasicBlock.java
@@ -1,5 +1,9 @@
 package top.origami404.ssyc.ir;
 
+import java.util.Map;
+
+import top.origami404.ssyc.ir.analysis.AnalysisInfo;
+import top.origami404.ssyc.ir.analysis.AnalysisInfoOwner;
 import top.origami404.ssyc.ir.inst.Instruction;
 import top.origami404.ssyc.ir.type.IRType;
 import top.origami404.ssyc.utils.IList;
@@ -8,7 +12,8 @@ import top.origami404.ssyc.utils.INode;
 import top.origami404.ssyc.utils.INodeOwner;
 
 public class BasicBlock extends Value
-    implements IListOwner<Instruction, BasicBlock>, INodeOwner<BasicBlock, Function> 
+    implements IListOwner<Instruction, BasicBlock>, INodeOwner<BasicBlock, Function>,
+        AnalysisInfoOwner
 {
     public BasicBlock() {
         super(IRType.BBlockTy);
@@ -24,6 +29,16 @@ public class BasicBlock extends Value
         return inode;
     }
 
+    public int getInstructionCount() {
+        return instructions.getSize();
+    }
+
+    @Override
+    public Map<String, AnalysisInfo> getInfoMap() {
+        return analysisInfos;
+    }
+
     private IList<Instruction, BasicBlock> instructions;
     private INode<BasicBlock, Function> inode;
+    private Map<String, AnalysisInfo> analysisInfos;
 }
