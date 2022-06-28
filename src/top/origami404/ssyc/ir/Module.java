9f6148463c0406a71360868a52250eb78942dc16
Origami404 <Origami404@foxmail.com>
2022-06-28 20:53:11 +0800

    ir: 更新了 Module 的 API

--- src/top/origami404/ssyc/ir/Module.java
+++ src/top/origami404/ssyc/ir/Module.java
@@ -1,35 +1,22 @@
 package top.origami404.ssyc.ir;
 
 import java.util.Map;
-import java.util.Optional;
 
 import top.origami404.ssyc.ir.Function;
 import top.origami404.ssyc.ir.constant.Constant;
 import top.origami404.ssyc.ir.inst.AllocInst;
 
 public class Module {
-    public Optional<Function> getFunction(String name) {
-        return Optional.ofNullable(functions.get(name));
+    public Map<String, Function> getFunctions() {
+        return functions;
     }
 
-    public Optional<AllocInst> getGlobalVar(String name) {
-        return Optional.ofNullable(variables.get(name));
+    public Map<String, Constant> getConstants() {
+        return constants;
     }
 
-    public Optional<Constant> getGlobalConst(String name) {
-        return Optional.ofNullable(constants.get(name));
-    }
-
-    public void putFunction(Function func) {
-        functions.put(func.getName(), func);
-    }
-
-    public void putGlobalVar(AllocInst var) {
-        variables.put(var.getName(), var);
-    }
-
-    public void putGlobalConst(String name, Constant constant) {
-        constants.put(name, constant);
+    public Map<String, AllocInst> getVariables() {
+        return variables;
     }
 
     private Map<String, Function> functions;
