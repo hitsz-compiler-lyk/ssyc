bb63049645f031d675737a68f3ad60e11c25eb78
Origami404 <Origami404@foxmail.com>
2022-07-05 23:24:05 +0800

    ir: 将 IR 内对参数的存储统一为一个 Value 数组, 对外提供统一的修改操作
    目前的 IR 操作无法做到简单的 RAUW (replace all use with) 操作. 其原因在于每个 IR 的 Operand 的结构是不同的, 只能做到统一查询, 无法做到统一修改.
    
    本 commit 将 IR 内对参数的存储统一为一个 Value 数组, 从而对外提供统一的修改操作.
    
    本 commit 不会改变目前的 IR 各参数的查询接口.

--- src/top/origami404/ssyc/ir/Value.java
+++ src/top/origami404/ssyc/ir/Value.java
@@ -1,12 +1,17 @@
 package top.origami404.ssyc.ir;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import top.origami404.ssyc.ir.inst.Instruction;
 import top.origami404.ssyc.ir.type.IRType;
 
 public abstract class Value {
     public Value(IRType type) {
         this.type = type;
+        this.userList = new ArrayList<>();
     }
 
     /**
@@ -16,9 +21,8 @@ public abstract class Value {
         return type;
     }
 
-
     public List<User> getUserList() {
-        return userList;
+        return Collections.unmodifiableList(userList);
     }
 
     public<T extends Value> boolean is(Class<T> cls) {
@@ -29,6 +33,30 @@ public abstract class Value {
         return cls.cast(this);
     }
 
+    public void replaceAllUseWith(Value newValue) {
+        userList.forEach(u -> u.replaceOperandCO(this, newValue));
+        newValue.userList = userList;
+        userList = new ArrayList<>();
+    }
+
+    public void replaceAllUseWithInBBlock(Value newValue, BasicBlock bblock) {
+        final var usersInBlock = userList.stream()
+            .filter(Instruction.class::isInstance)
+            .filter(i -> i.as(Instruction.class).getParent().isPresent())
+            .collect(Collectors.toUnmodifiableList());
+
+        for (final var user : usersInBlock) {
+            user.replaceOperandCO(this, newValue);
+            newValue.addUser(user);
+        }
+
+        userList.removeAll(usersInBlock);
+    }
+
+    public boolean isUseless() {
+        return userList.isEmpty();
+    }
+
     /**
      * 一个增加使用者的 "被动" 方法, 它只是朴素地加入一个 User, 不会 "主动" 维护 use-def 关系
      * @param user 待加入的 User
@@ -36,6 +64,7 @@ public abstract class Value {
     protected void addUser(User user) {
         userList.add(user);
     }
+    protected void removeUser(User user) { userList.remove(user); }
 
     private IRType type;
     private List<User> userList;
