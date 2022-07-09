640c5747124880a6a552be02cbcf26e8300ed858
Origami404 <Origami404@foxmail.com>
2022-07-09 18:50:20 +0800

    ir: 修正了一些关于 IRType 的错误, 同时改善了相应的接口
    BrInst 的 type 应为 Void 而不是 BBlock
    CmpInst 的 type 应为 Bool 而不是其参数的 type
    GEPInst 的 type 应恒为一指针

--- src/top/origami404/ssyc/ir/inst/BrInst.java
+++ src/top/origami404/ssyc/ir/inst/BrInst.java
@@ -6,7 +6,7 @@ import top.origami404.ssyc.ir.type.IRType;
 public class BrInst extends Instruction {
     public BrInst(BasicBlock nextBB) {
         // TODO: 维护基本块前后继关系
-        super(InstKind.Br, IRType.BBlockTy);
+        super(InstKind.Br, IRType.VoidTy);
         super.addOperandCO(nextBB);
 
         final var currBlock = this.getParent()
