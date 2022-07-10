02ad80e008c3209e11e3876222cc9758e5da7346
Origami404 <Origami404@foxmail.com>
2022-07-10 22:58:26 +0800

    ir-verify: 比较与运算类指令验证

--- src/top/origami404/ssyc/ir/inst/CmpInst.java
+++ src/top/origami404/ssyc/ir/inst/CmpInst.java
@@ -1,6 +1,8 @@
 package top.origami404.ssyc.ir.inst;
 
+import top.origami404.ssyc.ir.IRVerifyException;
 import top.origami404.ssyc.ir.Value;
+import top.origami404.ssyc.ir.constant.Constant;
 import top.origami404.ssyc.ir.type.IRType;
 
 public class CmpInst extends Instruction {
@@ -12,4 +14,27 @@ public class CmpInst extends Instruction {
 
     public Value getLHS() { return getOperand(0); }
     public Value getRHS() { return getOperand(1); }
+
+    @Override
+    public void verify() throws IRVerifyException {
+        super.verify();
+
+        final var lhs = getLHS();
+        final var rhs = getRHS();
+
+        final var thisType = getType();
+        final var lhsType = lhs.getType();
+        final var rhsType = rhs.getType();
+
+        ensure(thisType.isInt() || thisType.isFloat(),
+                "Type of a CmpInst must either be Int or be Float");
+        ensure((getKind().isInt() && thisType.isInt()) || (getKind().isFloat()) && thisType.isFloat(),
+                "Type of a CmpInst must match its kind");
+        ensure(thisType.equals(lhsType) && thisType.equals(rhsType),
+                "Type of LHS/RHS must be the same with the CmpInst");
+        ensure(getKind().isCmp(), "Unmatched kind");
+
+        ensureNot(lhs instanceof Constant && rhs instanceof Constant,
+                "A CmpInst shouldn't have two constants as its arguments");
+    }
 }
