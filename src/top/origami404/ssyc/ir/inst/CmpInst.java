bb18ab7e7926998a1467ba258c8086b1deb3a42b
Origami404 <Origami404@foxmail.com>
2022-07-10 22:44:07 +0800

    ir: 清除了不正确的 assert

--- src/top/origami404/ssyc/ir/inst/CmpInst.java
+++ src/top/origami404/ssyc/ir/inst/CmpInst.java
@@ -8,18 +8,8 @@ public class CmpInst extends Instruction {
         super(cmpKind, IRType.BoolTy);
         super.addOperandCO(lhs);
         super.addOperandCO(rhs);
-
-        final var resultTyKind = lhs.getType().getKind();
-        assert lhs.getType() == rhs.getType()
-            : "lhs should have the same type as rhs";
-        assert resultTyKind.isInt() || resultTyKind.isFloat()
-            : "BinOpInst require type INT or FLOAT";
-        assert (resultTyKind.isInt() && cmpKind.isInt()) || (resultTyKind.isFloat() && cmpKind.isFloat())
-            : "OpKind type is unmatch with operand type";
     }
 
-    public Value getLHS() {
-        return getOperand(0);
-    }
+    public Value getLHS() { return getOperand(0); }
     public Value getRHS() { return getOperand(1); }
 }
