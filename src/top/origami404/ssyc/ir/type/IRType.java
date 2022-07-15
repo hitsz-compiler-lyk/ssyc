34308745295ebc52573134777fb7a0ab060e3542
Origami404 <Origami404@foxmail.com>
2022-07-15 15:46:38 +0800

    ir: clearup: 移除了 IRType 中不必要的访问说明符与无用的方法

--- src/top/origami404/ssyc/ir/type/IRType.java
+++ src/top/origami404/ssyc/ir/type/IRType.java
@@ -2,44 +2,34 @@ package top.origami404.ssyc.ir.type;
 
 import java.util.List;
 
-public interface IRType {
-    public IRTyKind getKind();
+ public interface IRType {
+     IRTyKind getKind();
 
     /**
      * @return 该类型的对象占内存的大小 (字节)
      */
-    public int getSize();
+     int getSize();
 
-    public static final SimpleIRTy IntTy    = new SimpleIRTy(IRTyKind.Int, 4);
-    public static final SimpleIRTy FloatTy  = new SimpleIRTy(IRTyKind.Float, 4);
-    public static final SimpleIRTy VoidTy   = new SimpleIRTy(IRTyKind.Void);
-    public static final SimpleIRTy BoolTy   = new SimpleIRTy(IRTyKind.Bool, 1);
-    public static final SimpleIRTy BBlockTy = new SimpleIRTy(IRTyKind.BBlock);
+     SimpleIRTy IntTy    = new SimpleIRTy(IRTyKind.Int, 4);
+     SimpleIRTy FloatTy  = new SimpleIRTy(IRTyKind.Float, 4);
+     SimpleIRTy VoidTy   = new SimpleIRTy(IRTyKind.Void);
+     SimpleIRTy BoolTy   = new SimpleIRTy(IRTyKind.Bool, 1);
+     SimpleIRTy BBlockTy = new SimpleIRTy(IRTyKind.BBlock);
 
-    public static ArrayIRTy createArrayTy(int elementNum, IRType elementType) {
+     static ArrayIRTy createArrayTy(int elementNum, IRType elementType) {
         return new ArrayIRTy(elementNum, elementType);
     }
 
-    public static PointerIRTy createDecayType(ArrayIRTy type) {
+     static PointerIRTy createDecayType(ArrayIRTy type) {
         return IRType.createPtrTy(type.getElementType());
     }
 
-    public static PointerIRTy createPtrTy(IRType baseType) {
+     static PointerIRTy createPtrTy(IRType baseType) {
         return new PointerIRTy(baseType);
     }
 
-    public static FunctionIRTy createFuncTy(IRType returnType, List<IRType> paramTypes) {
+     static FunctionIRTy createFuncTy(IRType returnType, List<IRType> paramTypes) {
         return new FunctionIRTy(returnType, paramTypes);
-    }
-
-    /**
-     * 创建形如 [4 x i32]* 的类型, 即指向数组的指针类型, 一般用于类 C 语言的数组名的翻译
-     * @param elementNum 数组元素数量
-     * @param elementType 数组元素类型
-     * @return 所求的指针类型
-     */
-    public static PointerIRTy createArrayPtrTy(int elementNum, IRType elementType) {
-        return createPtrTy(createArrayTy(elementNum, elementType));
     }
 
     default boolean isInt()     { return this.equals(IRType.IntTy);     }
