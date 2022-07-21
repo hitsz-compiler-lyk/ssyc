b93fbadcc14c5240cf9a23a8ad04bb270072fca9
Origami404 <Origami404@foxmail.com>
2022-07-21 23:45:10 +0800

    ir: 加入对 IR 的 visitor

---
+++ src/top/origami404/ssyc/ir/visitor/ValueVisitor.java
@@ -0,0 +1,24 @@
+package top.origami404.ssyc.ir.visitor;
+
+import top.origami404.ssyc.ir.*;
+import top.origami404.ssyc.ir.inst.Instruction;
+import top.origami404.ssyc.ir.constant.Constant;
+
+public interface ValueVisitor<T> {
+    default T visit(Value value) {
+        if (value instanceof BasicBlock) { return visitBasicBlock((BasicBlock) value); }
+        if (value instanceof Function) { return visitFunction((Function) value); }
+        if (value instanceof GlobalVar) { return visitGlobalVar((GlobalVar) value); }
+        if (value instanceof Parameter) { return visitParameter((Parameter) value); }
+        if (value instanceof Instruction) { return visitInstruction((Instruction) value); }
+        if (value instanceof Constant) { return visitConstant((Constant) value); }
+        throw new RuntimeException("Unknown class: " + value.getClass().getSimpleName());
+    }
+
+    T visitBasicBlock(BasicBlock value);
+    T visitFunction(Function value);
+    T visitGlobalVar(GlobalVar value);
+    T visitParameter(Parameter value);
+    T visitInstruction(Instruction value);
+    T visitConstant(Constant value);
+}
