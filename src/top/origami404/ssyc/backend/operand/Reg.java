302d8f97ad65f217b9a197b440b4a3e5f4828214
youngk1019 <1186354278@qq.com>
2022-07-22 16:18:22 +0800

    add replaceRegAfter

--- src/top/origami404/ssyc/backend/operand/Reg.java
+++ src/top/origami404/ssyc/backend/operand/Reg.java
@@ -1,23 +1,23 @@
 package top.origami404.ssyc.backend.operand;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import top.origami404.ssyc.backend.arm.ArmInst;
 
 public abstract class Reg extends Operand {
-    private Set<ArmInst> instSet;
+    private List<ArmInst> instSet;
     private int id;
 
     public Reg(opType s) {
         super(s);
-        this.instSet = new HashSet<>();
+        this.instSet = new ArrayList<>();
     }
 
     public Reg(opType s, int id) {
         super(s);
         this.id = id;
-        this.instSet = new HashSet<>();
+        this.instSet = new ArrayList<>();
     }
 
     public abstract String toString();
@@ -38,7 +38,24 @@ public abstract class Reg extends Operand {
         instSet.remove(inst);
     }
 
-    public Set<ArmInst> getInstSet() {
+    public void replaceRegAfter(ArmInst inst, Reg reg) {
+        boolean isFound = false;
+        var replaceInsts = new ArrayList<ArmInst>();
+        for (var u : instSet) {
+            if (isFound) {
+                replaceInsts.add(u);
+            }
+            if (u.equals(inst)) {
+                isFound = true;
+            }
+        }
+
+        for (var u : replaceInsts) {
+            u.replaceOperand(this, reg);
+        }
+    }
+
+    public List<ArmInst> getInstSet() {
         return instSet;
     }
 
