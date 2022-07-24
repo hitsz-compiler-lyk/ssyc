f309d28f43d9bdd7e8a0f8cb3f2f9c2e5c1ab650
Origami404 <Origami404@foxmail.com>
2022-07-24 20:48:26 +0800

    ir: 整理基本块相关 API

--- src/top/origami404/ssyc/pass/ir/FunctionInline.java
+++ src/top/origami404/ssyc/pass/ir/FunctionInline.java
@@ -101,10 +101,10 @@ public class FunctionInline implements IRPass {
         final var splitResult = splitList(callerBlock.getIList(), callInst);
 
         // 处理 frontBB (该块的 Call 指令之前的指令要放的新块)
-        final var frontBB = new BasicBlock(callerFunc, callerBBSym.newSymbolWithSuffix(prefix + "_inline_front"));
+        final var frontBB = BasicBlock.createFreeBBlock(callerBBSym.newSymbolWithSuffix(prefix + "_inline_front"));
         // 接手原基本块 CallInst 前面的所有指令
         for (final var frontInst : splitResult.front) {
-            frontBB.getIList().add(frontInst);
+            frontBB.addInstAtEnd(frontInst);
         }
         callerBlock.adjustPhiEnd();
         // 接手原基本块的所有前继
@@ -119,7 +119,7 @@ public class FunctionInline implements IRPass {
         }
 
         // 处理 backBB (该块的 Call 指令之后的指令要放的新块)
-        final var backBB = new BasicBlock(callerFunc, callerBBSym.newSymbolWithSuffix(prefix + "_inline_back"));
+        final var backBB = BasicBlock.createFreeBBlock(callerBBSym.newSymbolWithSuffix(prefix + "_inline_back"));
         // 接手后继
         // 因为后继是通过 Br 指令获取的, 所以要在移除所有指令之前处理
         final var oldSuccs = new ArrayList<>(callerBlock.getSuccessors());
@@ -130,7 +130,7 @@ public class FunctionInline implements IRPass {
         // 接手原基本块 CallInst 后面的所有指令
         // 因为后继是通过 Br 指令获取的, 所以不需要特地维护 backBB 的后继列表
         for (final var backInst : splitResult.back) {
-            backBB.getIList().add(backInst);
+            backBB.addInstAtEnd(backInst);
         }
 
         // 要先将 call 指令从原块中移除
@@ -138,18 +138,18 @@ public class FunctionInline implements IRPass {
         callInst.freeFromIList();
 
         // 处理 exitBB (内联的函数的 "return" 指令要跳转到的块)
-        final var exitBB = new BasicBlock(callerFunc, callerBBSym.newSymbolWithSuffix(prefix + "_inline_exit"));
+        final var exitBB = BasicBlock.createFreeBBlock(callerBBSym.newSymbolWithSuffix(prefix + "_inline_exit"));
         final var returnType = calleeFunc.getType().getReturnType();
         if (!returnType.isVoid()) {
             // 构造用于存返回值的 Phi
             final var returnValueSym = new SourceCodeSymbol(calleeFunc.getFunctionSourceName() + prefix + "_inline_return", 0, 0);
             final var returnValuePhi = new PhiInst(exitBB, calleeFunc.getType().getReturnType(), returnValueSym);
-            exitBB.getIList().add(returnValuePhi);
+            exitBB.addInstAtEnd(returnValuePhi);
             // 将对 CallInst 的引用全部换成对返回值的 Phi 的引用
             callInst.replaceAllUseWith(returnValuePhi);
         }
         // 构造最后的跳转
-        exitBB.getIList().add(new BrInst(exitBB, backBB));
+        exitBB.addInstAtEnd(new BrInst(exitBB, backBB));
 
         // 开始函数内联
         // 获取形参到实参的映射
@@ -167,7 +167,7 @@ public class FunctionInline implements IRPass {
         final var calleeNewBlocks = cloner.convert();
 
         // 插入从 frontBB 到内联进来的函数的 entry 块的跳转
-        frontBB.getIList().add(new BrInst(frontBB, calleeNewBlocks.get(0)));
+        frontBB.addInstAtEnd(new BrInst(frontBB, calleeNewBlocks.get(0)));
         // 调整各个新块的 phiEnd
         frontBB.adjustPhiEnd();
         backBB.adjustPhiEnd();
@@ -229,7 +229,7 @@ public class FunctionInline implements IRPass {
             for (final var block : callee.getBasicBlocks()) {
                 final var newBlock = getOrCreate(block);
                 for (final var inst : block.allInst()) {
-                    newBlock.getIList().add(getOrCreate(inst));
+                    newBlock.addInstAtEnd(getOrCreate(inst));
                 }
 
                 newBlock.adjustPhiEnd();
@@ -254,7 +254,7 @@ public class FunctionInline implements IRPass {
                 return (BasicBlock) oldToNew.get(value);
             }
 
-            final var newBB = new BasicBlock(caller, value.getSymbol().newSymbolWithSuffix(prefix + "_inline"));
+            final var newBB = BasicBlock.createFreeBBlock(value.getSymbol().newSymbolWithSuffix(prefix + "_inline"));
             oldToNew.put(value, newBB);
             return newBB;
         }
