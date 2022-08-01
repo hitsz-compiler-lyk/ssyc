d770d8890790ef9526b327183f59fce7bbe81928
SoraShu <sorashu@outlook.com>
2022-08-01 20:28:54 +0800

    backend: 增加简单寄存器分配
    通过 `SimpleRegisterAssignment` 对象的 `getAssignMap`
    可实现寄存器分配并得到 Map

---
+++ src/top/origami404/ssyc/backend/codegen/MemPool.java
@@ -0,0 +1,51 @@
+package top.origami404.ssyc.backend.codegen;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MemPool {
+    public static final class Mem {
+        int offset;
+
+        Mem(int offset) {
+            this.offset = offset;
+        }
+
+        public int getOffset() {
+            return this.offset;
+        }
+    }
+
+    List<Mem> freeMem = new LinkedList<>();
+    List<Mem> takenMem = new LinkedList<>();
+    int size = 0;
+
+    public Mem add() {
+        final var mem = new Mem(size * 4);
+        freeMem.add(mem);
+        size++;
+        return mem;
+    }
+
+    public void put(Mem mem) {
+        if (takenMem.contains(mem)) {
+            takenMem.remove(mem);
+            freeMem.add(mem);
+        } else {
+            throw new RuntimeException("Put mem doesn't contain in takenMem");
+        }
+    }
+
+    public Mem take() {
+        if (freeMem.isEmpty()) {
+            add();
+        }
+        freeMem.sort(Comparator.comparingInt(Mem::getOffset));
+        return freeMem.remove(0);
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
