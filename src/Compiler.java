10aef56b5906441d1ee700cd831e14208871620f
Origami404 <Origami404@foxmail.com>
2022-08-04 04:30:34 +0800

    misc: 加入了对 OJ 评测情况下的日志支持

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,3 +1,5 @@
+import utils.Log;
+
 public class Compiler {
     public static void main(String[] args)  {
         // 功能测试：compiler -S -o testcase.s testcase.sy
@@ -7,6 +9,7 @@ public class Compiler {
             final var inputFileName = args[3];
             final var needOptimize = args.length == 6;
 
+            Log.inOnlineJudge();
             Main.main(new String[]{"asm", inputFileName, outputFileName});
 
         } catch (Exception e) {
