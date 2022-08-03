dfbb8d704041234e8cd36710928696eca2d72c68
Origami404 <Origami404@foxmail.com>
2022-08-04 04:23:57 +0800

    more-mem: fix: 修复了无用指令消除消除 phi 时不会调整基本块 phiEnd 的错误

--- src/pass/ir/ClearUselessInstruction.java
+++ src/pass/ir/ClearUselessInstruction.java
@@ -3,15 +3,27 @@ package pass.ir;
 import ir.Module;
 import ir.inst.CallInst;
 import ir.inst.Instruction;
+import ir.inst.PhiInst;
 
 public class ClearUselessInstruction implements IRPass {
     @Override
     public void runPass(Module module) {
-        IRPass.instructionStream(module).filter(this::canBeRemove).forEach(Instruction::freeAll);
+        IRPass.instructionStream(module).filter(this::canBeRemove).forEach(this::deleteInstruction);
     }
 
     public boolean canBeRemove(Instruction inst) {
         final var hasSideEffect = inst.getType().isVoid() || inst instanceof CallInst;
         return !hasSideEffect && inst.isUseless();
+    }
+
+    public void deleteInstruction(Instruction inst) {
+        // TODO: 思考是否将调整 block 的 phiEnd 的操作放到 phi 的 freeFromIList 里面
+        if (inst instanceof PhiInst) {
+            final var block = inst.getParent();
+            inst.freeAll();
+            block.adjustPhiEnd();
+        } else {
+            inst.freeAll();
+        }
     }
 }
