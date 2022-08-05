26ba1a54759e66c9ec2961cb7731de075e9f3ea8
Origami404 <Origami404@foxmail.com>
2022-08-05 20:00:37 +0800

    misc: 修复了 ANTLR 版本

--- src/frontend/SysYParser.java
+++ src/frontend/SysYParser.java
@@ -1,4 +1,4 @@
-// Generated from src/frontend/SysY.g4 by ANTLR 4.9.3
+// Generated from src/frontend/SysY.g4 by ANTLR 4.8
 
 package frontend;
 
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class SysYParser extends Parser {
-	static { RuntimeMetaData.checkVersion("4.9.3", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
