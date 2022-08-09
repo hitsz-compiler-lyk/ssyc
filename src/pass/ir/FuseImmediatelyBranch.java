d02cc16d2c0d0f448127b61107ad6215fc8cd7ce
Origami404 <Origami404@foxmail.com>
2022-08-09 16:09:07 +0800

    loop: fix: 修复了直接跳转消除的一个 bug
    如果 A -> B -> C 且 A -> C, 那么说明这个控制流会起到一个条件选择的作用
    
    这时候就不能直接合并它, 因为有可能在 C 开头会有 phi 借助控制流来求一个运行时根据条件才能确定的值

--- src/pass/ir/FuseImmediatelyBranch.java
+++ src/pass/ir/FuseImmediatelyBranch.java
@@ -20,6 +20,13 @@ public class FuseImmediatelyBranch implements IRPass {
         for (final var succ : IRPass.copyForChange(block.getSuccessors())) {
             if (succ.size() == 1 && succ.getPredecessorSize() == 1 && succ.getTerminator() instanceof BrInst) {
                 final var next = ((BrInst) succ.getTerminator()).getNextBB();
+
+                // 如果 A -> B -> C 且 A -> C, 那么说明这个控制流会起到一个条件选择的作用
+                // 这时候就不能直接合并它, 因为有可能在 C 开头会有 phi 借助控制流来求一个运行时根据条件才能确定的值
+                if (next.getPredecessors().contains(block)) {
+                    continue;
+                }
+
                 terminator.replaceOperandCO(succ, next);
                 next.replacePredecessor(succ, block);
 
