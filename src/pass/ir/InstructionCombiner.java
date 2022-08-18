2a80cfb4778f448f67daa7ef8942f5bb9b5e0104
SoraShu <sorashu@outlook.com>
2022-08-19 01:17:20 +0800

    inst-comb: 增加了连加转乘法的指令重排(不可用)

--- src/pass/ir/InstructionCombiner.java
+++ src/pass/ir/InstructionCombiner.java
@@ -7,12 +7,22 @@ import ir.constant.IntConst;
 import ir.inst.BinaryOpInst;
 import ir.inst.InstKind;
 import ir.inst.Instruction;
+import utils.Log;
+import utils.Pair;
 
-import java.util.Set;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class InstructionCombiner implements IRPass {
     @Override
     public void runPass(Module module) {
+        instRepeatInfoMap = new HashMap<>();
+        IRPass.instructionStream(module)
+                .map(inst->new Pair<>(inst,getRepeatInfo(inst)))
+                .forEach(this::repeatedAddComb);
         new ConstructDominatorInfo().runPass(module);
         IRPass.instructionStream(module)
             .filter(this::matchMultiOp)
@@ -21,6 +31,8 @@ public class InstructionCombiner implements IRPass {
         IRPass.instructionStream(module).forEach(this::swapConst);
         IRPass.instructionStream(module).forEach(this::biOpWithZeroOneComb);
     }
+
+    private Map<Instruction,RepeatInfo> instRepeatInfoMap;
 
     private boolean isKind(Value value, InstKind kind) {
         if (value instanceof Instruction) {
@@ -164,6 +176,126 @@ public class InstructionCombiner implements IRPass {
                     inst.freeAll();
                 }
             }
+        }
+    }
+    private static class RepeatInfo {
+        private boolean isRepeatedAdd;
+        /**
+         * 是否 Top 节点, 即是否需要展开
+         */
+        private boolean isTop;
+        /**
+         * key 需为 Instruction 或 IntConst
+         */
+        private Map<Value, Integer> valueFrequencyMap;
+        RepeatInfo(boolean isRepeatedAdd,boolean isTop) {
+            this.isRepeatedAdd = isRepeatedAdd;
+            this.isTop = isTop;
+            this.valueFrequencyMap = new HashMap<>();
+        }
+        void put(Value key,Integer value) {
+            Log.ensure((key instanceof IntConst) || (key instanceof Instruction));
+            valueFrequencyMap.put(key, value);
+        }
+
+        void merge(Value key,Integer value) {
+            valueFrequencyMap.merge(key, value, Integer::sum);
+        }
+
+        void mergeAll(RepeatInfo info) {
+            info.valueFrequencyMap.forEach(this::merge);
+        }
+    }
+
+    private boolean isIntConst(Value value) {
+        return value instanceof IntConst;
+    }
+
+    /**
+     * 需要记录的 value 只有 inst 和 Const
+     * @param inst 待记录的 instruction
+     * @return info
+     */
+    private RepeatInfo getRepeatInfo(Instruction inst) {
+        if (instRepeatInfoMap.containsKey(inst)) {
+            return instRepeatInfoMap.get(inst);
+        }
+        if (inst instanceof BinaryOpInst) {
+            final var binst = (BinaryOpInst) inst;
+            final var lhs = binst.getLHS();
+            final var rhs = binst.getRHS();
+            if (isKind(inst, InstKind.IAdd)) {
+                final var info = new RepeatInfo(true, true);
+                if (lhs instanceof Instruction) {
+                    info.mergeAll(getRepeatInfo((Instruction) lhs));
+                } else if (lhs instanceof IntConst) {
+                    info.merge(lhs, 1);
+                } else {
+                    Log.ensure(false,"lhs must be a Instruction or IntConst.");
+                }
+                if (rhs instanceof Instruction) {
+                    info.mergeAll(getRepeatInfo((Instruction) rhs));
+                } else if (rhs instanceof IntConst) {
+                    info.merge(rhs, 1);
+                } else {
+                    Log.ensure(false,"rhs must be a Instruction or IntConst.");
+                }
+                instRepeatInfoMap.put(inst, info);
+                return info;
+            } else if (isKind(inst, InstKind.IMul)) {
+                if (isIntConst(lhs) && !isIntConst(rhs)) {
+                    final var info = new RepeatInfo(true, false);
+                    final var con = (IntConst) lhs;
+                    info.put(rhs,con.getValue());
+                    instRepeatInfoMap.put(inst, info);
+                    return info;
+                }
+                if (!isIntConst(lhs) && isIntConst(rhs)) {
+                    final var info = new RepeatInfo(true, false);
+                    final var con = (IntConst) rhs;
+                    info.put(rhs, con.getValue());
+                    instRepeatInfoMap.put(inst, info);
+                    return info;
+                }
+            }
+        }
+
+        final var info = new RepeatInfo(false, false);
+        info.put(inst, 1);
+        instRepeatInfoMap.put(inst, info);
+        return info;
+    }
+
+    private void repeatedAddComb(Pair<Instruction,RepeatInfo> pair) {
+        final var inst = pair.getKey();
+        final var info = pair.getValue();
+        if (info.isTop && info.isRepeatedAdd) {
+            final var entryList = info.valueFrequencyMap.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue()).collect(Collectors.toList());
+            final List<Value> valueList = new LinkedList<>();
+            for (var entry : entryList) {
+                if (entry.getValue() > 0 && entry.getValue() < 5) {
+                    for (int i = 0; i < entry.getValue(); i++) {
+                        valueList.add(entry.getKey());
+                    }
+                } else if (entry.getValue() >= 5) {
+                    final var mulInst = new BinaryOpInst(InstKind.IMul, entry.getKey(),
+                            Constant.createIntConstant(entry.getValue()));
+                    inst.insertBeforeCO(mulInst);
+                    valueList.add(mulInst);
+                }
+            }
+            Log.ensure(valueList.size() >= 2, "valueList must have two or more elements.");
+            final var flhs = valueList.remove(0);
+            final var frhs = valueList.remove(0);
+            Instruction lastInst = new BinaryOpInst(InstKind.IAdd, flhs, frhs);
+            for (var value : valueList) {
+                inst.insertBeforeCO(lastInst);
+                lastInst = new BinaryOpInst(InstKind.IAdd, value, lastInst);
+            }
+            inst.replaceAllUseWith(lastInst);
+            inst.replaceInIList(lastInst);
+            inst.freeAll();
         }
     }
 }
